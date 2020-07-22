fd411361a74006f3722427796a8946e34e501c36
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-22 13:48:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Student
-
+DB = {:conn => SQLite3::Database.new("db/students.db")}
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
